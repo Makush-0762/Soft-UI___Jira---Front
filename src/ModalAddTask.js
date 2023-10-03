8a8b236b44135c5410d4ec54beab87fc07374ab8
Kushka Roman <romanovich9102@gmail.com>
2023-10-03 17:06:57 +0300

    Накидав основу для модалки додавання таску #2

--- src/ModalAddTask.js
+++ src/ModalAddTask.js
@@ -17,21 +17,32 @@ export default function ModalAddTask ({}){
     return(
             <Div className="bodyModalAddTask" >
                 <Div className="blockEnterDataTask">
-                    <h2 title="Внесіть дані до відповідних полів">Створення таску</h2>
-                    <Form className="bodyInputEnter">
-                        <Label className="labelEnterTaskName" htmlFor="name">Введіть опис до таску, або ж посилання</Label>
-                        {/* <Input className="inputEnterTaskName" id="name" placeholder="Назва таску"/> */}
-                        <textarea className="textareaEnterTaskName" id="name"></textarea>
-                    </Form>
-                    <Form>
-                        <select className="selectModalAddTask">
-                            <option>До виконання</option>
-                            <option>До виконання</option>
-                            <option>До виконання</option>
-                            <option>До виконання</option>
-                        </select>
-                        <Input className="inputEnterTaskUser" id="user_id" value="1" type="hidden" />
-                    </Form>
+                    <Div className="blockEnterData_item testDataItem">
+                        <h2 title="Внесіть дані до відповідних полів">Створення таску</h2>
+                    </Div>
+                    <Div className="blockEnterData_item testDataItem">
+                        <Form className="bodyInputEnter">
+                            <Label className="labelEnterTaskName" htmlFor="name">Введіть опис до таску, або ж посилання</Label>
+                            {/* <Input className="inputEnterTaskName" id="name" placeholder="Назва таску"/> */}
+                            <textarea className="textareaEnterTaskName" id="name"></textarea>
+                        </Form>
+                    </Div>
+                    <Div className="blockEnterData_item">
+                        <Form>
+                            <Label className="labelEnterTaskName" htmlFor="status_id">Статус</Label>
+                            <select className="selectModalAddTask" id="status_id">
+                                <option>До виконання</option>
+                                <option>До виконання</option>
+                                <option>До виконання</option>
+                                <option>До виконання</option>
+                            </select>
+                            <Input className="inputEnterTaskUser" id="user_id" value="1" type="hidden" />
+                        </Form>
+                    </Div>
+                    <Div className="actionDataModalTask blockEnterData_item">
+                        <ButtonGroup >Закрити</ButtonGroup>
+                        <ButtonGroup >Додати</ButtonGroup>
+                    </Div>
                 </Div>
             </Div>
     );
